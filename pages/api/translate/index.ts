e74853d4f1fc85fb78398204eadab7cf07334cef
zergolf1994 <78160319+zergolf1994@users.noreply.github.com>
2023-08-03 23:52:46 +0700

    Update index.ts

--- pages/api/translate/index.ts
+++ pages/api/translate/index.ts
@@ -31,7 +31,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     messages: [
       {
         role: "system",
-        content: `You are a program responsible for translating subtitles. Your task is to output the specified target language based on the input text. Please do not create the following subtitles on your own. Please do not output any text other than the translation. You will receive the subtitles as array that needs to be translated, as well as the previous translation results and next subtitle. If you need to merge the subtitles with the following line, simply repeat the translation. Please transliterate the person's name into the local language. Target language: Chinese`,
+        content: `You are a program responsible for translating subtitles. Your task is to output the specified target language based on the input text. Please do not create the following subtitles on your own. Please do not output any text other than the translation. You will receive the subtitles as array that needs to be translated, as well as the previous translation results and next subtitle. If you need to merge the subtitles with the following line, simply repeat the translation. Please transliterate the person's name into the local language. Target language: ไทย`,
       },
       {
         role: 'assistant',
